a35c84ac8a4c90a28abd764bfab0e6653a586a4f
Johan14030 <115721882+Johan14030@users.noreply.github.com>
2024-10-18 13:15:25 +0200

    test
    test

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d99c32e183a38c202c8e13566db048bca0d8cbfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5176e38b9e33641fccf3d9b5be157dd1a8d0a2a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

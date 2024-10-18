d99c32e183a38c202c8e13566db048bca0d8cbfd
Johan14030 <115721882+Johan14030@users.noreply.github.com>
2024-10-18 13:07:55 +0200

    gitignore test

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89b8510c175bcd22293625db35860984969491b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2c4623c01799c8f45885badaf11e11d5aed5f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

154de7048eb8a0eaa52fe69453d8315a39e8b36d
Hadle <43191424+HadleMS@users.noreply.github.com>
2024-10-22 15:47:36 +0200

    Added a logo

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792e336c795972cb4d84d5d44956557bfe5af7f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9311f253ce61243437b31bc5cf74a2ac53db5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

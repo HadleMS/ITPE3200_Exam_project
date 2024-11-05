8c3b3dbe56834eeb2f2c9699df67d954bd954e63
Hadle <43191424+HadleMS@users.noreply.github.com>
2024-11-05 14:45:09 +0100

    Updates the Navbar
    updated navbar and added scrollwheel to items page

--- obj/Debug/net8.0/MyShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31480a663d74d5a116e256675ec2953ec314cd04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6fb328b01569fdec888dbae3a33f4947c2807a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
